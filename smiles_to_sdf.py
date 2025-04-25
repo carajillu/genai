97a88602278acfa26c503b59ae5c06dda9aaa532
carajillu <joan.clark88@gmail.com>
2025-04-25 11:42:34 +0100

    script to generate sdf and pdbqt from SMILES

---
+++ smiles_to_sdf.py
@@ -0,0 +1,65 @@
+import argparse
+from rdkit import Chem
+from rdkit.Chem import AllChem
+import os
+import subprocess
+import sys
+
+def parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--smiles_str", type=str, nargs="?", help="Smiles string", default=None)
+    parser.add_argument("--smiles_path", type=str, nargs="?", help="Smiles file", default=None)
+    args = parser.parse_args()
+    assert args.smiles_str is not None or args.smiles_path is not None, "Smiles string or file must be provided"
+    return args
+
+def smiles_to_3D(smiles, mol_name):
+    print(smiles)
+    """
+    Convert a smiles string to an sdf file
+    """
+    mol = Chem.MolFromSmiles(smiles)
+    mol.SetProp('_Name', mol_name)
+    molh=Chem.AddHs(mol)
+    AllChem.EmbedMolecule(molh,AllChem.ETKDG())
+    AllChem.UFFOptimizeMolecule(molh,maxIters=1000)
+    return molh
+
+def sdf_to_pdbqt(sdf_path,pdbqt_path):
+    """
+    Uses subprocess to call openbabel to convert an sdf file to a pdbqt file
+    Using subprocess cause babel wrappel is not working on macos.
+    """
+    subprocess.run(["obabel", "-isdf", sdf_path, "-opdbqt", "-O", pdbqt_path])
+    return pdbqt_path
+
+if __name__ == "__main__":
+    args = parse()
+    print(args)
+    if args.smiles_path is not None:
+        mol=[]
+        smiles_list = []
+        with open(args.smiles_path, "r") as f:
+            for line in f:
+                if line.strip() == "":
+                    continue
+                smiles_list.append(line.strip())
+        #print(smiles_list)
+        for i in range(len(smiles_list)):
+            smiles = smiles_list[i]
+            mol_name = f"molecule_{i}"
+            try:
+                mol.append(smiles_to_3D(smiles, mol_name))
+            except Exception as e:
+                print(f"Error converting smiles to 3D: {e}")
+                continue
+    elif args.smiles_str is not None:
+        mol = [smiles_to_3D(args.smiles_str, "molecule_0")]
+        
+    os.makedirs("sdf", exist_ok=True)
+    os.makedirs("pdbqt", exist_ok=True)
+    for molecule in mol:
+        Chem.MolToMolFile(molecule, f"sdf/{molecule.GetProp('_Name')}.sdf")
+        pdbqt_path = sdf_to_pdbqt(f"sdf/{molecule.GetProp('_Name')}.sdf", f"pdbqt/{molecule.GetProp('_Name')}.pdbqt")
+        print(pdbqt_path)
+        
