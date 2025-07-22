0e408d16fcb469359eaa65b2be0267c98fbc6d36
Joan Clark-Nicolas <joan.clark88@gmail.com>
2025-07-22 11:34:10 +0100

    Needs tidied up but it runs

--- dockflow/cli/parallel_docking.py
+++ dockflow/cli/parallel_docking.py
@@ -3,6 +3,8 @@ import sys
 import subprocess
 import argparse
 import pandas as pd
+import numpy as np
+import glob
 import mdtraj
 from dockflow.dock.prepare.ligands import prepare_library_from_smiles
 from dockflow.dock.prepare.receptors import gen_binding_site_pdb, gen_receptor_pdbqt
@@ -10,9 +12,11 @@ from dockflow.dock.vina import gen_vina_config, batch_dock
 from dockflow.analyze.contact import pdbqt_to_mdtraj_trajectory, compute_contact_surface_area_single_pose
 from dockflow.analyze.vina_scores import scores_from_pdbqt
 from dockflow.analyze.rmsd import compute_rmsds_against_ref
+from meeko import PDBQTMolecule
 def parse_args():
     parser = argparse.ArgumentParser(description="Run parallel docking")
-    parser.add_argument("--receptor",nargs="+", type=str, required=True, help="Path to the receptor PDB file")
+    parser.add_argument("--receptor", type=str, help="Path to the receptor PDB file if only one")
+    parser.add_argument("--receptor_dir", type=str, default=None, help="Path to the directory containing receptor PDB files")
     parser.add_argument("--library_smi", type=str, default=None, help="Path to the ligand library of smiles files")
     parser.add_argument("--bsite_selection", type=str, required=True, help="Region of the receptor we are going to dock in")
     parser.add_argument("--ref_ligand", type=str, default=None, help="If relevant, reference ligand for RMSD calculation")
@@ -21,14 +25,34 @@ def parse_args():
     args = parser.parse_args()
     if args.library_smi is None and args.ref_ligand is None:
         raise ValueError("Either --library_smi or --ref_ligand must be provided")
+    if args.receptor is None and args.receptor_dir is None:
+        raise ValueError("Either --receptor or --receptor_dir must be provided")
+
     return args
 
 if __name__=="__main__":
     
     args=parse_args()
-    pdbqt_library_path=prepare_library_from_smiles(args.library_smi, output_dir="pdbqt_library")
+
+    # Read SMILES file and prepare library
+    smiles_df=pd.read_csv(args.library_smi)
+    assert 'smiles' in smiles_df.columns or 'SMILES' in smiles_df.columns, \
+        "SMILES file must contain a column named 'smiles' or 'SMILES'."
+    smiles_list = smiles_df['smiles'] if 'smiles' in smiles_df.columns else smiles_df['SMILES']
+
+    pdbqt_library_path=prepare_library_from_smiles(smiles_list, output_dir="pdbqt_library")
+    best_scores=[np.inf]*len(smiles_list) # Initialize best scores for each ligand
+    receptor_conformations=[None]*len(smiles_list) # receptor pdb that docks the best ligand pose
+
+    # get receptor paths
+    if args.receptor_dir is not None:
+        receptor_paths = glob.glob(os.path.join(args.receptor_dir, "*.pdb"))
+    elif args.receptor is not None:
+        receptor_paths = [args.receptor]
+    
     root_dir = os.getcwd()
-    for receptor in args.receptor:
+    for receptor in receptor_paths:
+        # Prepare receptor path and name
         receptor_path = os.path.abspath(receptor)
         receptor_name = receptor.split("/")[-1].split(".")[0]
         os.makedirs(receptor_name, exist_ok=True)
@@ -43,15 +67,22 @@ if __name__=="__main__":
         
         
         # dock the ligands in each receptor
-        vina_output_path=batch_dock(vina_exec="~/github/Vina-GPU-2.1/AutoDock-Vina-GPU-2.1/AutoDock-Vina-GPU-2-1", receptor_pdbqt=f"{receptor_name}.pdbqt", 
+        batch_dock(vina_exec="~/github/Vina-GPU-2.1/AutoDock-Vina-GPU-2.1/AutoDock-Vina-GPU-2-1", receptor_pdbqt=f"{receptor_name}.pdbqt", 
                               ligand_directory=pdbqt_library_path, config=f"{receptor_name}.box.txt",
                               thread=8000,opencl_binary_path="/home/joan/github/Vina-GPU-2.1/AutoDock-Vina-GPU-2.1",output_directory="./results")
         
-        pose_bust(vina_output_path) # run posebusters on all poses, discard the ones that don't pass
+        #pose_bust(vina_output_path) # run posebusters on all poses, discard the ones that don't pass
 
-        results=get_results(vina_output_path, score_threshold, results) # get the results of each docking and append them to a DataFrame
-
+        for i in range(len(smiles_list)):
+            ligand_out= f"results/ligand_{i}_out.pdbqt"
+            poses=PDBQTMolecule.from_file(ligand_out)
+            score=poses.score # that returns the score from the first pose, which is the best one
+            if score < best_scores[i]:
+                best_scores[i] = score
+                receptor_conformations[i] = receptor_name
         os.chdir(root_dir)
-    sys.exit()
-    best_results=consolidate_results() # remove redundant smiles, keep the best scoring ones, discard compounds above similarity threshold (?)
-    best_results.to_csv("best_results.csv", index=False)  # Save the consolidated results
+    
+    
+    smiles_df['best_score'] = best_scores
+    smiles_df['receptor'] = receptor_conformations
+    smiles_df.to_csv(args.results, index=False)
